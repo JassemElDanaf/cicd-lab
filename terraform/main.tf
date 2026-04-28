b6767bcd752974eca2c1b0ae9598c973216d7730
Jassem El Danaf <jassem@example.com>
2026-04-28 20:37:23 +0300

    Add George Abou Assale as co-owner on demo S3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ resource "aws_s3_bucket" "demo" {
     Environment = var.environment
     ManagedBy   = "terraform"
     Lab         = "010"
-    Owner       = "Jassem El Danaf"
+    Owner       = "Jassem El Danaf, George Abou Assale"
   }
 }
 
